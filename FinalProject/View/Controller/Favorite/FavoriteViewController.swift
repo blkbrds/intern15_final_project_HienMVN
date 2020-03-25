ba37ef6c1fe96f08fedbb529cd6d80aa5eb3491d
at-hienmai <hien.mai@asiantech.vn>
2020-03-25 16:25:22 +0700

    setupUI for FavoriteScreen

--- FinalProject/View/Controller/Favorite/FavoriteViewController.swift
+++ FinalProject/View/Controller/Favorite/FavoriteViewController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 final class FavoriteViewController: ViewController {
 
+	@IBOutlet weak var tableView: UITableView!
 	// MARK: - Life Cycle
 	override func viewDidLoad() {
 		super.viewDidLoad()
@@ -9,5 +10,40 @@ final class FavoriteViewController: ViewController {
 
 	override func setupUI() {
 		title = "Favorite"
+		let nib = UINib(nibName: Config.favoriteTableViewCell, bundle: .main)
+		tableView.register(nib, forCellReuseIdentifier: Config.favoriteTableViewCell)
+		tableView.dataSource = self
+		tableView.delegate = self
+		let deleteAllBarButtonItem = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deleteAll))
+		self.navigationItem.rightBarButtonItem = deleteAllBarButtonItem
+	}
+	@objc func deleteAll() {
+
+	}
+}
+
+extension FavoriteViewController: UITableViewDataSource {
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return Config.numberOfRow
+	}
+
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		guard let cell = tableView.dequeueReusableCell(withIdentifier: Config.favoriteTableViewCell, for: indexPath) as? FavoriteTableViewCell else {
+			return UITableViewCell(style: .default, reuseIdentifier: Config.favoriteTableViewCell)
+		}
+		return cell
+	}
+}
+
+extension FavoriteViewController: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		return Config.heightForRow
+	}
+}
+extension FavoriteViewController {
+	struct Config {
+		static var favoriteTableViewCell = "FavoriteTableViewCell"
+		static var numberOfRow: Int = 10
+		static var heightForRow: CGFloat = 100
 	}
 }
---
+++ FinalProject/View/Controller/Favorite/TableViewCell/FavoriteTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  FavoriteTableViewCell.swift
+//  FinalProject
+//
+//  Created by Ngoc Hien on 3/25/20.
+//  Copyright © 2020 Asian Tech Inc.,. All rights reserved.
+//
+
+import UIKit
+
+class FavoriteTableViewCell: UITableViewCell {
+	@IBOutlet weak var addressLabel: UILabel!
+	@IBOutlet weak var favoriteImageView: UIImageView!
+	@IBOutlet weak var nameLabel: UILabel!
+	override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
